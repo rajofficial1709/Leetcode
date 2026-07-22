f172890f536a562e4b74ee748ddec14bac74392e
rajofficial1709 <raj.official1709@gmail.com>
2026-07-22 19:05:38 +0530

    Time: 1 ms (100%), Space: 42.8 MB (44.37%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,6 +1,18 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long m= (long)n;
-        return (int)(Math.sqrt(8*m+1)-1)/2;
+       int low=1,hig=n,ans=0;
+        while(low<=hig){
+             int mid=low+(hig-low)/2;
+             long coins=(long)mid*(mid+1)/2;
+            if(coins<=n){
+                ans=mid;
+                low=mid+1;
+             }
+            else{
+                hig=mid-1;
+            }
+        } 
+      
+       return (int)ans;
     }
 }
