44a62c5e0954f5f890661089f53e29a7ea38dce6
rajofficial1709 <raj.official1709@gmail.com>
2026-07-26 13:20:24 +0530

    Time: 2 ms (99.31%), Space: 44.1 MB (92.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.replaceAll("[^a-zA-Z0-9]","");
-        s=s.toLowerCase();
         int i=0,j=s.length()-1;
         while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+             while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
                 return false;
             }
             i++;
             j--;
         }
         return true;
-
     }
 }
